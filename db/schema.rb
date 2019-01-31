f52826a9e7d977010cf71938597d702c92ce559e
vincelep <vincent.leparoux@gmail.com>
2019-01-31 18:46:40 +0100

    version finale AirBnDog :)

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_01_31_172014) do
 
   create_table "city_strolls", force: :cascade do |t|
+    t.string "city_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -20,14 +21,18 @@ ActiveRecord::Schema.define(version: 2019_01_31_172014) do
   create_table "dog_sitters", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.integer "city_stroll_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["city_stroll_id"], name: "index_dog_sitters_on_city_stroll_id"
   end
 
   create_table "dogs", force: :cascade do |t|
     t.string "name"
+    t.integer "city_stroll_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["city_stroll_id"], name: "index_dogs_on_city_stroll_id"
   end
 
   create_table "strolls", force: :cascade do |t|
