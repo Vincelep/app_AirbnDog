7ab7343256f904203cf5fea74161ff3ddcffd054
vincelep <vincent.leparoux@gmail.com>
2019-01-31 17:37:49 +0100

    ajout des models

---
+++ app/models/dog_sitter.rb
@@ -0,0 +1,4 @@
+class DogSitter < ApplicationRecord
+	has_many :strolls
+	belongs_to 
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+10.times do |index|
+	dog_sitter = Dog_sitter.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
+
+10.times do |index|
+	dog = Dog.create!(name: Faker::Pokemon.unique.name)
+end	
+
+10.times do |index|
+	stroll = Stroll.create!(dog_sitter_id: (rand(Dog_sitter.first.id..Dog_sitter.last.id)), dog_id:(rand(Dog.first.id..Dog.last.id)), date: Faker::Date.forward(23))
+end	
